b423db735391cf3d71c9fd42eecb59f25cdbfd09
Veomaany <veomany.khammarath@gmail.com>
2018-10-31 00:27:41 +1100

    add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :conversations do
+    resources :messages
+  end
+
   devise_for :users
-  root 'home#index'
+  root 'conversations#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
